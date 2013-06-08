7dc149536cf74f782a1216d3ebc96c73762a2f17
Alisa Dammer <alisa.dammer@gmail.com>
2013-06-08 03:54:33 +0200

    class bus is extended

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ MAP_WIDTH = WIDTH * 4
 MAP_HEIGHT = HEIGHT * 4
 
 PERIOD_OF_TIME = 0
+COUGHT_A_BUS = False
+busses = []
 # create the main window
 window = sf.RenderWindow(sf.VideoMode(WIDTH, HEIGHT), "pySFML Window")
 
@@ -18,8 +20,8 @@ class Actor(sf.Drawable):
 
         self.velocity = 1
 
-    def move(self, delta):
-        self.sprite.position += delta * self.velocity
+    def move(self, dx, dy):
+        self.sprite.position += (dx, dy) * self.velocity
 
     def draw(self, target, states):
         target.draw(self.sprite, states)
@@ -37,88 +39,86 @@ class Player(Actor):
     def draw(self, target, states):
         target.draw(self.sprite, states)
 
+    def got_bus(self, Bus):
+        COUGHT_A_BUS = True
+        
+
+player = Player()
+
+
 class Bus(Actor):
     def __init__(self, start_number):
         Actor.__init__(self)
-        self.start_number = 0
+
+        self.start_number = PERIOD_OF_TIME
 
         self.sprite = sf.RectangleShape()
         self.sprite.size = (50, 50)
         self.sprite.outline_color = sf.Color.BLUE
-        self.sprite.outline_thickness = 2
+        slef.sprite.outline_thickness = 2
         self.sprite.position = (342, 100)
 
     def draw(self, target, states):
         target.draw(self.sprite, states)
 
+    def bus_move(self):
+        if (self.position.x > 342 and self.position.y > 0):
+            self.move(self.position.x, self.position.y)
+        else:
+            self.dissapear()
+
     def get_number(self):
-        self.start_number = PERIOD_OF_TIME
+        return self.start_number
 
-busses = []
+    def dessapear(self):
+        busses.pop(len(0))
 
-player = Player()
+        
 
 background_texture = sf.Texture.from_file("map1.png")
 background = sf.Sprite(background_texture)
 
 view = sf.View()
 view.reset(sf.Rectangle((0, 0), (WIDTH, HEIGHT)))
+window.view = view
 
 timer = sf.Clock()
 # start the game loop
 while window.is_open:
-
     if timer.elapsed_time >= sf.seconds(15):
         PERIOD_OF_TIME += 1
         timer.restart()
         print("Period: " + str(PERIOD_OF_TIME))
-
         bus = Bus(PERIOD_OF_TIME)
-
+        print("Bus's numbers are: " + str(bus.get_number()))
+        busses.append(bus)
+        
 # process events
     for event in window.events:
         if type(event) is sf.CloseEvent:
             timer.restart()
             PERIOD_OF_TIME = 0
+            busses = []
             window.close()
 
-    delta = sf.Vector2()
     if sf.Keyboard.is_key_pressed(sf.Keyboard.LEFT) and player.sprite.position.x > 0:
-        delta += (-1,0)
-    elif sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT) and player.sprite.position.x < MAP_WIDTH:
-        delta += (1,0)
-        
+        player.sprite.position += (-1,0)
+    elif sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT) and player.sprite.position.x < WIDTH:
+        player.sprite.position += (1,0)
+
     if sf.Keyboard.is_key_pressed(sf.Keyboard.UP) and player.sprite.position.y > 0:
-        delta += (0,-1)
-    elif sf.Keyboard.is_key_pressed(sf.Keyboard.DOWN) and player.sprite.position.y < MAP_HEIGHT:
-        delta += (0,1)
+        player.sprite.position += (0,-1)
+    elif sf.Keyboard.is_key_pressed(sf.Keyboard.DOWN) and player.sprite.position.y < HEIGHT:
+        player.sprite.position += (0,1)
         
     elif sf.Keyboard.is_key_pressed(sf.Keyboard.ESCAPE):
         window.close()
-
-    if sf.Keyboard.is_key_pressed(sf.Keyboard.L_SHIFT):
-        player.velocity = 8
-    else:
-        player.velocity = 2
-
-    print(delta)
-    player.move(delta)
-    view.move(delta.x * player.velocity, delta.y * player.velocity)
     
-    window.view = view
 
     window.clear() # clear screen
     window.draw(background)
     window.draw(player) # draw the sprite
     for bus in busses:
         window.draw(bus)
-
-    window.view = window.default_view
-    debug_text = sf.Text("Pos: %s, Period: %i" % (player.sprite.position, PERIOD_OF_TIME))
-    debug_text.color = sf.Color.RED
-    debug_text.position = (0, HEIGHT - 20)
-    debug_text.character_size = 12
-    window.draw(debug_text)
-
     window.display() # update the window
 
